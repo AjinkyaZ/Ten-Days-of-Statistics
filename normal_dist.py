6777b6fef1c047417ced6fd2bc73e46b4dd9f235
ajinkya.z <ajinkya.z@edgenetworks.in>
2016-10-03 16:31:48 +0530

    Solved normal distribution and poisson problems

---
+++ normal_dist.py
@@ -0,0 +1,48 @@
+from math import sqrt
+
+def error_function(z):
+    e = 2.71828
+    # Using Horner's method since hackerrank does not allow importing scipy.integrate.quad :(
+    t = 1.0/(1.0 + 0.5*abs(z))
+    expo = e**(-z*z - 1.26551223 + t * ( 1.00002368 + t * ( 0.37409196 + t * ( 0.09678418 + t * (-0.18628806 + t * ( 0.27886807 + t * (-1.13520398 + t * ( 1.48851587 + t * (-0.82215223 + t * ( 0.17087277))))))))))
+    x = 1 - t*expo
+    if (z >= 0):
+        return  x
+    else: 
+        return -x
+    
+def cumulative_dist_normal(x, mu, sigma):
+    error_input = (x-mu)/(sigma*sqrt(2))
+    return 0.5*(1 + error_function(error_input))
+
+def main_1():
+    #Hackerrank problem on normal distribution
+    print "Problem Statement\nGiven time to assemble a car as a random variable X having normal distribution mean of A hrs,\nand standard devation of B hours, find probability that a car can be assembled\nwithin a given set of bounds."
+    d = [float(i) for i in raw_input("Enter Mean and Std.Dev for X:\n").split(" ")]
+    mean_x = d[0]
+    sd_x = d[1]
+    r = [float(i) for i in raw_input("Enter lower and upper bounds for probable number of hours to assemble a car:\n").split(" ")]
+    lower_b = r[0]
+    upper_b = r[1]
+    print "Probability that a car can be assembled within the given time constraints is {0:.3f}".format(cumulative_dist_normal(upper_b, mean_x, sd_x)-cumulative_dist_normal(lower_b, mean_x, sd_x))
+
+
+def main_2():
+    #Another Hackerrank problem on normal distribution
+    print "Problem Statement\nGiven mean and std.dev for Physics grades for a group of students,\nand the distinction and passing bounds, find percentage of students who score above the distinction threshold,\nthose who pass, and those who fail."
+    (mean, sd) = [int(i) for i in raw_input("Enter Mean and Std.Dev for student grades:\n").split(" ")]
+    l1 = int(raw_input("Enter distiniction threshold:\n"))
+    l2 = int(raw_input("Enter passing threshold:\n"))
+    print "Percentage of students who obtain distinction is {0:.2f}%".format((1-cumulative_dist_normal(l1, mean, sd))*100)
+    print "Percentage of students who pass is {0:.2f}%".format((1-cumulative_dist_normal(l2, mean, sd))*100)
+    print "Percentage of students who fail is {0:.2f}%".format((cumulative_dist_normal(l2, mean, sd))*100)
+
+
+def main():
+    main_1()
+    print "\n\n***********************************\n\n"
+    main_2()
+
+
+if __name__ == "__main__":
+    main()
--- poisson_dist.py
+++ poisson_dist.py
@@ -14,7 +14,7 @@ def poisson_dist(l, k):
     e = 2.71828
     return (((l)**k)*(e**-l))/(facto(k))
 
-def main1():
+def main_1():
     #Hackerrank problem on poisson distribtion
     print "Problem Statement:\nGiven mean of variable following the Poisson distribution,\nfind probability of it being equal to N."
     a = float(raw_input("Enter mean for variable :\n"))
@@ -22,8 +22,26 @@ def main1():
     print "Probability of the variable being equal to {0:d} is {1:.3f}".format(b,poisson_dist(a, b))
 
 
+def main_2():
+    #Another Hackerrank problem on poisson distribution
+    print "Problem Statement:\nNumber of repairs for two machines A and B is a poisson random var, with means of x and y respectively"
+    print "Given x and y, find Expected daily cost to repair each machine, where\na) Cost for A = 160+40(X*X)\nb) Cost for B = 128+40(Y*Y)"
+    means = [float(i) for i in raw_input("Enter means for A and B's number of repairs\n").split(" ")]
+    mean_a = means[0]
+    mean_b = means[1]
+    print  "Since number of repairs for both A and B are poisson random variables,\nE[X] = mean, Var(X) = mean and E[X*X] = Var(X) + E[X]*E[X]"
+    E_Xsq_a = mean_a + (mean_a)**2
+    E_Xsq_b = mean_b + (mean_b)**2
+    expected_cost_a = 160+40*E_Xsq_a
+    expected_cost_b = 128+40*E_Xsq_b
+    print "Expected daily cost of A is {0:.3f}".format(expected_cost_a)
+    print "Expected daily cost of B is {0:.3f}".format(expected_cost_b)
+
+
 def main():
-    main1()
+    main_1()
+    print "\n\n***********************************\n\n"
+    main_2()
 
 
 if __name__ == "__main__":
